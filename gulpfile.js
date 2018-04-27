a773ecb48ca4305c216798af1487bed939552cf1
langer-nice <davidlangNCE@gmail.com>
2018-04-27 16:55:16 +0200

    xx

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+
+gulp.task('default', function(){
+   console.log('hooray - you just created a Gulp task'); 
+});
+
+gulp.task('html', function(){
+   console.log('Imaging something useful being done to the HTML here'); 
+});
+
+gulp.task('styles', function(){
+   console.log('Imaging SASS or postCSS tasks being done here'); 
+});
+
+gulp.task('watch', function(){
+    
+   watch('./app/index.html', function(){
+       gulp.start('html');
+   });
+    
+    watch('./app/assets/styles/**/*.css'), function(){
+        gulp.start('styles');
+    };
+});
