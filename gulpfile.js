0df507f8a7ca49fed33d9105ee267e4af2c5945c
langer-nice <davidlangNCE@gmail.com>
2018-04-30 17:29:43 +0200

    css architecture added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssVars = require('postcss-simple-vars'),
+cssNested = require('postcss-nested'),
+cssImport = require('postcss-import');
+
+
+
 
 gulp.task('default', function(){
    console.log('hooray - you just created a Gulp task'); 
@@ -9,17 +17,21 @@ gulp.task('html', function(){
    console.log('Imaging something useful being done to the HTML here'); 
 });
 
+
 gulp.task('styles', function(){
-   console.log('Imaging SASS or postCSS tasks being done here'); 
+   return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssImport, cssVars, cssNested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function(){ 
     
-   watch('./app/index.html', function(){
+    watch('./app/index.html', function(){
        gulp.start('html');
-   });
-    
+    });
+
     watch('./app/assets/styles/**/*.css', function() {
         gulp.start('styles');
     });
+    
 });
