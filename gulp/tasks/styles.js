d8938466b8a7a15155d328de3f0d1773c5fd610d
langer-nice <davidlangNCE@gmail.com>
2018-05-14 21:45:34 +0200

    reponsive images stage

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,12 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssVars = require('postcss-simple-vars'),
 cssNested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
    return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssVars, cssNested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssVars, cssNested, autoprefixer]))
         .on('error', function(errorInfo){
             console.log(errorInfo.toString());
             this.emit('end');    
